7a342636c03759cc8d8ff532998607b034237773
Kenny2github <kenny2minecraft@gmail.com>
2019-09-08 21:22:09 +0800

    Keyed Morsing
    * Monkeypatch discord.gateway for SPEAKING
      events to use as keyer
    * Use wave averaging to combine tones
    * Muting is now done by removing all tones
    - all automatic Morse, including
      - play_morse
      - Room.speak
      - all WPM stuff
      - auto-welcomes
      - documentation saying "send morse"
      - automatically Morsing room name
    + listen to monkeypatched `speaking_update`
      events for keying
    * Fix: only done in a net
    * Fix: only done if currently speaking
    * Fix: only done if not the host
    * users is no longer host-only
    - Remove wait before connecting because there
      isn't any auto-morse thing anymore

--- main.py
+++ main.py
@@ -4,8 +4,40 @@ import itertools
 import traceback
 import asyncio
 import discord
+import discord.gateway
 import discord.opus
 from discord.ext import commands
+
+#monkeypatch voice speaking op
+async def received_message(self, msg):
+    op = msg['op']
+    data = msg.get('d')
+
+    if op == self.READY:
+        await self.initial_connection(data)
+    elif op == self.HEARTBEAT_ACK:
+        self._keep_alive.ack()
+    elif op == self.INVALIDATE_SESSION:
+        await self.identify()
+    elif op == self.SESSION_DESCRIPTION:
+        self._connection.mode = data['mode']
+        await self.load_secret_key(data)
+    elif op == self.HELLO:
+        interval = data['heartbeat_interval'] / 1000.0
+        self._keep_alive = discord.gateway.VoiceKeepAliveHandler(ws=self, interval=interval)
+        self._keep_alive.start()
+    elif op == self.SPEAKING:
+        user_id = int(data['user_id'])
+        vc = self._connection
+
+        if vc.guild:
+            user = vc.guild.get_member(user_id)
+        else:
+            user = client.get_user(user_id)
+
+        client.dispatch('speaking_update', user, data['speaking'])
+
+discord.gateway.DiscordVoiceWebSocket.received_message = received_message
 
 client = commands.Bot('=', description='Communicate with Morse code')
 
@@ -51,18 +83,20 @@ async def on_command_error(ctx, exc):
 FREQ = 48000
 
 class Wave(discord.AudioSource):
-    muted = False
 
     def __init__(self, freq=440):
-        self.freq = self._freq = freq
+        self.freq = freq
+        self.freqs = set()
         self.wav = self.wave()
 
     def wave(self):
-        return ((i % int(FREQ / self.freq) / FREQ) for i in itertools.count())
+        return (
+            (sum(i % int(FREQ / f) / FREQ for f in self.freqs)
+            / len(self.freqs)) if self.freqs else 0
+            for i in itertools.count()
+        )
 
     def read(self):
-        if self.muted:
-            return b'\0' * 3840
         samples = itertools.islice(self.wav, int(0.02 * FREQ))
         return b''.join(
             int(i * 32767 + 32768).to_bytes(2, 'big') * 2
@@ -125,54 +159,14 @@ MORSE = {
 def morse_msg(msg):
     return ' '.join(MORSE.get(i, '..--..') for i in msg.lower())
 
-def mute(sources, yes):
-    for r in sources:
-        r.muted = yes
-
-async def play_morse(msg, sources, wpm, freq):
-    ditlength = 6/(5 * max(15, wpm))
-    msg = '/'.join(
-        ' '.join(
-            '_'.join(
-                i for i in j.strip()
-                if i in '.-'
-            )
-            for j in k.strip().split()
-        )
-        for k in msg.split('/')
-    )
-    for r in sources:
-        r.freq = freq
-    for i in msg:
-        if i == '.':
-            mute(sources, False)
-            await asyncio.sleep(ditlength)
-        if i == '-':
-            mute(sources, False)
-            await asyncio.sleep(ditlength * 3)
-        if i == '_':
-            mute(sources, True)
-            await asyncio.sleep(ditlength)
-        if i == ' ':
-            mute(sources, True)
-            await asyncio.sleep(18 / (5 * wpm))
-        if i == '/':
-            mute(sources, True)
-            await asyncio.sleep(42 / (5 * wpm))
-    mute(sources, True)
-    for r in sources:
-        r.freq = r._freq
-
 class Room:
     """Represents a Morse room."""
-    def __init__(self, name, wpm=15, net=False, password=None):
+    def __init__(self, name, net=False, password=None):
         self.name = name
-        self.wpm = wpm
         self.net = net
         self.password = password
         self.wavs = {}
         self.futs = {}
-        self.wpms = {}
         self.speaking = None
         self.new_users = set()
         self.host = None
@@ -194,6 +188,25 @@ class Room:
 
     async def handle_msgs(self):
         """Background task that handles all messages."""
+        @client.listen()
+        async def on_speaking_update(user, state):
+            try:
+                for c in self.wavs:
+                    if c.author == user:
+                        ctx = c
+                        break
+                else:
+                    return
+                if self.net and self.speaking != ctx:
+                    return
+                for c in self.wavs:
+                    r = self.wavs[c]
+                    if state & 1:
+                        r.freqs.add(self.wavs[ctx].freq)
+                    else:
+                        r.freqs.discard(self.wavs[ctx].freq)
+            except:
+                traceback.print_exc()
         try:
             while 1:
                 ctx, msg = await self.messages.get()
@@ -207,81 +220,58 @@ class Room:
                         self.call(i)
                         for i in self.wavs
                     ))
-                    continue
-                if msg.content == 'done':
+                elif self.net and msg.content == 'done':
                     if msg.author.id != ctx.author.id:
+                        continue
+                    if self.speaking == self.host:
+                        continue
+                    if self.speaking != ctx:
                         continue
                     print(now(), ctx.author, 'ran', msg.content)
                     await self.set_speaker(ctx)
-                    continue
-                if msg.content.startswith('wpm'):
-                    if msg.author.id != ctx.author.id:
-                        continue
-                    print(now(), ctx.author, 'ran', msg.content)
-                    try:
-                        self.wpms[ctx] = int(msg.content.split()[-1])
-                    except ValueError:
-                        continue
-                    await ctx.send('WPM set to %s' % self.wpms[ctx])
-                    continue
-                if msg.content == 'bye':
+                elif msg.content == 'bye':
                     if msg.author.id != ctx.author.id:
                         continue
                     print(now(), ctx.author, 'ran', msg.content)
                     self.futs[ctx].set_result(None)
-                if not msg.content.startswith(('.', '-')):
-                    continue
-                if self.net and not msg.author.id == ctx.author.id:
-                    continue
-                await self.speak(msg.content, self.wavs[ctx], self.wpms[ctx])
-                await asyncio.sleep(1)
         except asyncio.CancelledError:
             return
         except:
             traceback.print_exc()
+        finally:
+            client.remove_listener('speaking_update', on_speaking_update)
 
-    async def connect(self, ctx, wav, fut, wpm):
+    async def connect(self, ctx, wav, fut):
         """Connects the wave to the room. Returns whether you are the host."""
         self.wavs[ctx] = wav
         self.futs[ctx] = fut
-        self.wpms[ctx] = wpm
-        morsing = asyncio.create_task(play_morse(
-            morse_msg(self.name), [wav], 30, 440
-        ))
         if self.net:
             await ctx.send(f"""Joined your voice channel.
 You are connected to **net** `{self.name}`.
 Wait until you are allowed to speak.
-Once you may, send `-- --- .-. ... . / -.-. --- -.. .`
-to play it to all connected users.""")
+Once you may, use PTT to key Morse code.""")
             if len(self.wavs) == 1:
                 await self.set_host(ctx)
                 await self.set_speaking(ctx)
-                await morsing
                 return True
             self.new_users.add(ctx)
             await ctx.send(f'The net host is {self.host.author!s} \
 ({self.call(self.host)}). Feel free to DM them with any concerns.')
-            await self.host.send(f'{self.call(ctx)} just joined the net! Run \
-`welcome` to process automatic welcomes.')
         else:
             await ctx.send(f"""Joined your voice channel.
 You are connected to room `{self.name}`.
-Send `-- --- .-. ... . / -.-. --- -.. .`
-to play it to all connected users in the room.""")
-            await asyncio.gather(*(
-                i.send(f'{self.call(ctx)} just joined the room!')
-                for i in self.wavs
-                if i != ctx
-            ))
-        await morsing
+Use PTT to key Morse code.""")
+        await asyncio.gather(*(
+            i.send(f'{self.call(ctx)} just joined the room!')
+            for i in self.wavs
+            if i != ctx
+        ))
         return False
 
     async def disconnect(self, ctx):
         """Disconnects the wave from the room. Returns whether to destroy."""
         del self.wavs[ctx]
         del self.futs[ctx]
-        del self.wpms[ctx]
         self.new_users.discard(ctx)
         if self.net:
             if self.host == ctx and len(self.wavs) > 0:
@@ -294,34 +284,6 @@ to play it to all connected users in the room.""")
             self.task.cancel()
             return True
         return False
-
-    async def welcome(self):
-        """Starts welcoming new users."""
-        if not self.net:
-            return
-        orig_speaking = self.speaking
-        self.speaking = None
-        await self.speak(morse_msg('Welcome:'), None, freq=440)
-        await asyncio.sleep(0.5)
-        for i in self.new_users:
-            await self.speak(
-                morse_msg(self.call(i)),
-                None, freq=440
-            )
-            await asyncio.sleep(0.5)
-        self.speaking = orig_speaking
-        self.new_users.clear()
-
-    async def speak(self, content, wav, wpm=None, freq=None):
-        """Speaks content in Morse. If not allowed to speak, fails silently."""
-        if self.net and self.wavs.get(self.speaking, None) != wav:
-            return
-        await play_morse(
-            content,
-            list(self.wavs.values()),
-            wpm or self.wpm,
-            freq or wav._freq
-        )
 
     async def set_speaking(self, ctx):
         """Host-only: sets the user who may speak."""
@@ -356,14 +318,6 @@ if i != self.host and i != old_host))
         if ctx != self.host or msg.author.id != self.host.author.id:
             return False
         content = msg.content
-        if content.startswith('net wpm'):
-            print(now(), ctx.author, 'ran', msg.content)
-            try:
-                self.wpm = int(content.split(' ')[-1])
-            except ValueError:
-                pass
-            await ctx.send('Net WPM set to ' + str(self.wpm))
-            return True
         if content.startswith('host'):
             print(now(), ctx.author, 'ran', msg.content)
             call = content.split()[-1]
@@ -384,18 +338,6 @@ if i != self.host and i != old_host))
                     return True
             await ctx.send('No user with that callsign')
             return True
-        if content.startswith('welcome'):
-            print(now(), ctx.author, 'ran', msg.content)
-            await ctx.send('Welcoming new users')
-            old_wpm = self.wpm
-            if content != 'welcome':
-                try:
-                    self.wpm = int(content.split()[-1])
-                except ValueError:
-                    return True
-            await self.welcome()
-            self.wpm = old_wpm
-            return True
         return False
 
 class Morse(commands.Cog):
@@ -413,34 +355,28 @@ class Morse(commands.Cog):
 
     @commands.command()
     async def join(
-        self, ctx, room, wpm: int = 15,
+        self, ctx, room,
         net: bool = False, password: bool = False
     ):
         """Join or create a room and send Morse. PLEASE RUN `=help join`
 
-        If the room does not exist, it will be created. Send a message
-        consisting solely of Morse code (or at least starting with a . or -) to
-        interpret the message as Morse and send it at your WPM. Because the bot
-        can only connect to one voice channel per server, anyone can join the
-        voice channel with the bot and send Morse (unless the room is a net - if
-        that is the case, others can only listen in); however, only the user who
-        joined the bot to the voice channel may use "transient commands":
+        If the room does not exist, it will be created. Use PTT to key Morse
+        code to everyone in the room or net. Because the bot can only connect to
+        one voice channel per server, anyone can join the voice channel with the
+        bot, but only the person joined the bot to the voice channel may key
+        Morse or use "transient commands":
 
         done: transfers speaking privileges back to the host
-        wpm <N>: changes the transmit WPM to N.
         bye: disconnects the bot
+        users: returns a list of callsigns connected to the net
 
         Nets are a more formal type of room where one person (the host) decides
         who is allowed to speak at any given moment. This is useful for large
         rooms where it would be too chaotic if everyone were to speak at once.
         Only the host of a net may use the following transient commands:
 
-        net wpm <N>: changes the WPM for automatic welcome messages.
         host <callsign>: transfers hosting privileges to the specified user
         speaker <callsign>: transfers speaking privileges to the specified user
-        welcome [WPM]: starts the process of automatically welcoming users who
-            have joined since the last welcome, optionally at WPM WPM
-        users: returns a list of callsigns connected to the net
 
         If `net` is true and the room did not previously exist, the room will be
         created as a net, with you as the host. If `password` is true, you will
@@ -480,7 +416,7 @@ class Morse(commands.Cog):
             return
         _room = room
         if room not in rooms:
-            rooms[room] = Room(room, wpm, net, password)
+            rooms[room] = Room(room, net, password)
             if rooms[room].net and password:
                 await ctx.send('Please DM me the new password for this net.')
                 msg = await client.wait_for('message', check=lambda m: (
@@ -506,7 +442,6 @@ class Morse(commands.Cog):
                 await ctx.send("You didn't get the password right in three \
 tries! Please try again later.")
                 return
-        await asyncio.sleep(1.5) # wait a bit because it takes a bit to start
         fut = client.loop.create_future()
         @client.listen()
         async def on_message(m):
@@ -515,7 +450,7 @@ tries! Please try again later.")
             if m.content.startswith('='):
                 return
             room.messages.put_nowait((ctx, m))
-        await room.connect(ctx, wav, fut, wpm)
+        await room.connect(ctx, wav, fut)
         await fut
         client.remove_listener('message', on_message)
         vc.stop()
